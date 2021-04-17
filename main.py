6d49c1697d22485925731e1bd60054531276a6de
Emma Cranston <cranston.emma@gmail.com>
2021-04-16 20:52:45 -0700

    Trying new method for repeatable querying

--- main.py
+++ main.py
@@ -7,22 +7,74 @@ client = commands.Bot(command_prefix=".")
 token = os.getenv("TOKEN")
 db = os.getenv("DATABASE_URL")
 
+def check_role(ctx):
+  role = discord.utils.get(ctx.guild.roles, name="Bingo Dictator")
+  if role in ctx.author.roles:
+    return True
+  else:
+    return False
+
+def run_query(query_sql, success_msg='Success', fail_msg='Failed'):
+  try:
+      print("connecting to database")
+      conn = psycopg2.connect(db, sslmode='require')
+      cursor = conn.cursor()
+      cursor.execute(query_sql)
+      conn.commit()
+      return success_msg
+    except psycopg2.errors.UniqueViolation:
+      return fail_msg
+    except:
+      return "Error inserting value to database"
+    finally:
+      if(conn):
+        cursor.close()
+        conn.close()
+
 @client.event
 async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
 
-# @client.command()
-# async def ping(ctx) :
-#     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+@client.command(name="BingoAdd")
+async def BingoAdd(ctx) :
+  item = ctx.message.content
+  query_sql = f"""INSERT INTO public.bingolist (key, server)
+  VALUES ('{item}', '{ctx.guild.name}' )"""
+  addQuery = run_query(query_sql
+    , f"Added {item} to bingolist."
+    , f"{item} is already in the bingo list."
+    )
+  await ctx.send(addQuery)
 
-# @client.command(name="whoami")
-# async def whoami(ctx) :
-#     await ctx.send(f"You are {ctx.message.author.name}")
+@client.command(name="bingoadd")
+async def bingoadd(ctx) :
+    """ Adds an item to the bingo list.
+     #TODO 
+     * add game czar check before bingo is added
+     * and a sarcastic message if non-bingo czar tries it ("Nice try, PEASANT!")
+    """
+    print(ctx.guild.name)
+    item = ctx.message.content
+    query_sql = f"""INSERT INTO public.bingolist (key, server)
+    VALUES ('{item}', '{ctx.guild.name}' )"""
+    try:
+      print("connecting to database")
+      conn = psycopg2.connect(db, sslmode='require')
+      cursor = conn.cursor()
+      cursor.execute(query_sql)
+      conn.commit()
+      await ctx.send(f"Added {item} to bingolist.")
+    except psycopg2.errors.UniqueViolation:
+      await ctx.send(f"{item} is already in the bingo list!")
+    except:
+      await ctx.send("Error inserting value to database")
+    finally:
+      if(conn):
+        cursor.close()
+        conn.close()
+        await ctx.send("Cursor Closed")
 
-# @client.command()
-# async def clear(ctx, amount=3) :
-#     await ctx.channel.purge(limit=amount)
 
 @client.command(name="bingolist")
 async def bingolist(ctx) :
@@ -58,33 +110,35 @@ async def bingolist(ctx) :
         conn.close()
         print("cursor closed")
 
-@client.command(name="bingoadd")
-async def bingoadd(ctx) :
-    """ Adds an item to the bingo list.
-     #TODO 
-     * add game czar check before bingo is added
-     * and a sarcastic message if non-bingo czar tries it ("Nice try, PEASANT!")
+@client.command(name="currentlist")
+async def currentlist(ctx) :
+    """ Shows the current list of items that have been confirmed by the Bingo Dictator.
+    #TODO
+     * add server protection check
     """
-    print(ctx.guild.name)
-    item = ctx.message.content
-    query_sql = f"""INSERT INTO public.bingolist (key, server)
-    VALUES ('{item}', '{ctx.guild.name}' )"""
+    query_sql = "SELECT * FROM public.current_game WHERE confirmed = TRUE;"
     try:
-      print("connecting to database")
       conn = psycopg2.connect(db, sslmode='require')
       cursor = conn.cursor()
       cursor.execute(query_sql)
-      conn.commit()
-      await ctx.send(f"Added {item} to bingolist.")
-    except psycopg2.errors.UniqueViolation:
-      await ctx.send(f"{item} is already in the bingo list!")
+      records = cursor.fetchall()
+
+      record_string = ''
+      for row in records:
+        rowvals = ''
+        for item in row:
+          rowvals += str(item) + ', '
+          print(rowvals)
+        record_string += rowvals + "\n"
+      await ctx.send(f"All data: ```{record_string}```")
+
     except:
-      await ctx.send("Error inserting value to database")
+      print("Error connecting to database")
+
     finally:
       if(conn):
         cursor.close()
         conn.close()
-        await ctx.send("Cursor Closed")
 
 @client.command(name="found")
 async def found(ctx):
@@ -141,36 +195,6 @@ async def confirmFind(ctx, item):
     pass
   else:
     await ctx.send("Simple _peasants_ mayn't confirm Bingo finds.")
-
-@client.command(name="currentlist")
-async def currentlist(ctx) :
-    """ Shows the current list of items that have been confirmed by the Bingo Dictator.
-    #TODO
-     * add server protection check
-    """
-    query_sql = "SELECT * FROM public.current_game WHERE confirmed = TRUE;"
-    try:
-      conn = psycopg2.connect(db, sslmode='require')
-      cursor = conn.cursor()
-      cursor.execute(query_sql)
-      records = cursor.fetchall()
-
-      record_string = ''
-      for row in records:
-        rowvals = ''
-        for item in row:
-          rowvals += str(item) + ', '
-          print(rowvals)
-        record_string += rowvals + "\n"
-      await ctx.send(f"All data: ```{record_string}```")
-
-    except:
-      print("Error connecting to database")
-
-    finally:
-      if(conn):
-        cursor.close()
-        conn.close()
 
 
 client.run(token)
