860d9885f1ef5a0292525f1ea8944a10e8446518
Emma Cranston <cranston.emma@gmail.com>
2021-04-16 18:44:02 -0700

    added todos

--- main.py
+++ main.py
@@ -12,43 +12,17 @@ async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
 
-@client.command()
-async def ping(ctx) :
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+# @client.command()
+# async def ping(ctx) :
+#     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
 
-@client.command(name="whoami")
-async def whoami(ctx) :
-    await ctx.send(f"You are {ctx.message.author.name}")
+# @client.command(name="whoami")
+# async def whoami(ctx) :
+#     await ctx.send(f"You are {ctx.message.author.name}")
 
-@client.command()
-async def clear(ctx, amount=3) :
-    await ctx.channel.purge(limit=amount)
-
-@client.command(name="bingodata")
-async def bingodata(ctx):
-    query_sql = "SELECT * FROM public.bingolist;"
-    try:
-      conn = psycopg2.connect(db, sslmode='require')
-      cursor = conn.cursor()
-      cursor.execute(query_sql)
-      records = cursor.fetchall()
-
-      record_string = ''
-      for row in records:
-        rowvals = ''
-        for item in row:
-          rowvals += str(item) + ', '
-          print(rowvals)
-        record_string += rowvals + "\n"
-      await ctx.send(f"All data: ```{record_string}```")
-
-    except:
-      print("Error connecting to database")
-
-    finally:
-      if(conn):
-        cursor.close()
-        conn.close()
+# @client.command()
+# async def clear(ctx, amount=3) :
+#     await ctx.channel.purge(limit=amount)
 
 @client.command(name="bingolist")
 async def bingolist(ctx) :
@@ -74,7 +48,6 @@ async def bingolist(ctx) :
       print(f"(log) Bingo list includes... {record_string}")
       await ctx.send(f"**Bingo list includes:** ```\n{record_string}```")
 
-
     except:
       print("Error connecting to database")
 
@@ -85,8 +58,12 @@ async def bingolist(ctx) :
         print("cursor closed")
 
 @client.command(name="bingoadd")
-async def bingoadd(ctx, item) :
+""" #TODO add game czar check before bingo is added
+     * and a sarcastic message if non-bingo czar tries it
+"""
+async def bingoadd(ctx) :
     print(ctx.guild.name)
+    item = ctx.message.content
     query_sql = f"""INSERT INTO public.bingolist (key, server)
     VALUES ('{item}', '{ctx.guild.name}' )"""
     try:
@@ -108,6 +85,7 @@ async def bingoadd(ctx, item) :
 
 @client.command(name="found")
 async def found(ctx):
+  # TODO: make sure the item name is in the card items list
     await ctx.send(f"You said: {ctx.message.content}")
     msg = ctx.message.content.split(',')
     key = msg[0]
@@ -132,10 +110,55 @@ async def found(ctx):
         conn.close()
         print("Cursor Closed")
 
+@client.command(name="newGame")
+async def newGame(ctx):
+  """ 
+  #TODO 
+   * swap public.current_list with public.prior_list
+   * make a new blank list in public.current_list out of the available entries in bingolist
+      * link and confirmation will be blank
+   * make a new game in public.current_game, wipe old players list to public.last_game
+  """
+  pass
+
+@client.command(name="confirmFind")
+async def confirmFind(ctx, item):
+  """ 
+  #TODO
+   * update public.current_list to reflect confirmations from game czar
+   * add sarcastic message if non-czar tries it
+  """
+  pass
 
 @client.command(name="currentlist")
 async def currentlist(ctx) :
-    await ctx.send(f"You said: {ctx.message.content}")
+  """
+  #TODO
+   * add server protection check
+  """
+    query_sql = "SELECT * FROM public.current_game WHERE confirmed = TRUE;"
+    try:
+      conn = psycopg2.connect(db, sslmode='require')
+      cursor = conn.cursor()
+      cursor.execute(query_sql)
+      records = cursor.fetchall()
+
+      record_string = ''
+      for row in records:
+        rowvals = ''
+        for item in row:
+          rowvals += str(item) + ', '
+          print(rowvals)
+        record_string += rowvals + "\n"
+      await ctx.send(f"All data: ```{record_string}```")
+
+    except:
+      print("Error connecting to database")
+
+    finally:
+      if(conn):
+        cursor.close()
+        conn.close()
 
 
 client.run(token)
