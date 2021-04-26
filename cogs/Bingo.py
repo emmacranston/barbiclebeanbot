f4559467abb2578f387488da1b702f86d674cfa7
Emma Cranston <cranston.emma@gmail.com>
2021-04-26 15:42:35 -0700

    add commands.command instead of client.commands?

--- cogs/Bingo.py
+++ cogs/Bingo.py
@@ -6,20 +6,20 @@ class Bingo(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 
-	@client.command(name="bl")
-	async def bl(ctx) :
-	    """Lists the items used in this bingo game's card generator."""
-	    print(ctx.guild.name)
-	    query_sql = f"""
-	    SELECT DISTINCT key 
-	    FROM public.bingolist
-	    WHERE server = '{ctx.guild.name}'
-	    ;"""
+		@commands.command(name="bl")
+		async def bl(ctx) :
+		    """Lists the items used in this bingo game's card generator."""
+		    print(ctx.guild.name)
+		    query_sql = f"""
+		    SELECT DISTINCT key 
+		    FROM public.bingolist
+		    WHERE server = '{ctx.guild.name}'
+		    ;"""
 
-	    listQuery = pull_query(query_sql,
-	      ' ')
-	    print(listQuery)
-	    await ctx.send(f"**Bingo list includes:** ```\n{listQuery}```")
+		    listQuery = pull_query(query_sql,
+		      ' ')
+		    print(listQuery)
+		    await ctx.send(f"**Bingo list includes:** ```\n{listQuery}```")
 
 def setup(client):
 	client.add_cog(Bingo(client))
